ab310f445032c37f16eaf57edfdb3b38e0b5743b
MaGnsio <magnsi0o0@gmail.com>
2025-03-01 17:06:23 +0200

    treesitter 🌴

--- lua/mg/plugins/treesitter.lua
+++ lua/mg/plugins/treesitter.lua
@@ -2,46 +2,31 @@ return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
+  dependencies = { "windwp/nvim-ts-autotag", },
   config = function()
     -- import nvim-treesitter plugin
     local treesitter = require("nvim-treesitter.configs")
 
     -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
+    treesitter.setup({
+      -- install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = true,
+      -- automatically install missing parsers when entering buffer
+      auto_install = false,
+      -- list of parsers to ignore installing (or "all")
+      ignore_install = {},
+      modules = {},
+      -- enable syntax highlighting
+      highlight = { enable = true, },
       -- enable indentation
       indent = { enable = true },
       -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
+      autotag = { enable = true, },
       -- ensure these language parsers are installed
-      ensure_installed = {
-        "c",
-        "cpp",
-        "python",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "lua",
-        "gitignore",
-        "query",
-        "vim",
-        "vimdoc",
-      },
+      ensure_installed = { "c", "cpp", "python", "go", "rust", "markdown", "markdown_inline", "bash", "lua", "gitignore", "query", "vim", "vimdoc", },
       incremental_selection = {
         enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
+        keymaps = { init_selection = "<C-space>", node_incremental = "<C-space>", scope_incremental = false, node_decremental = "<bs>", },
       },
     })
   end,
