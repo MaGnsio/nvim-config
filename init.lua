946302ee150accfe2a2a82df97ad966e52322d31
MaGnsio <magnsi0o0@gmail.com>
2025-03-01 02:05:36 +0200

    add startup timestamp ⏳

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
+local start_time = vim.loop.hrtime()  -- Get the start time (in nanoseconds)
+
 require("mg.core")
 require("mg.lazy")
+
+local loading_time = math.floor((vim.loop.hrtime() - start_time) / 1e6 + 0.5)
+print("Startup Time: " .. loading_time .. "ms ⏳")
