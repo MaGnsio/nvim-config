cec0bb38dcc1c67a4d2f04d87e6e8f49eb8970bf
MaGnsio <magnsi0o0@gmail.com>
2025-03-04 01:51:41 +0200

    format and linting 🔗

--- lua/mg/core/keymaps.lua
+++ lua/mg/core/keymaps.lua
@@ -13,21 +13,27 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- incremen
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })                   -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })                 -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })                    -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })               -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })                     -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })              -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })                     --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                 --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- lsp helpful keymaps
 vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
 vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
 vim.keymap.set('n', 'grr', vim.lsp.buf.references)
+vim.keymap.set('n', "grf", function() vim.lsp.buf.format({
+    filter = function(client)
+        print("Formatting with: " .. client.name)
+        return true  -- This ensures all formatters are listed
+    end,
+}) end)
 
 -- disable automatic clipboard copying when deleting or changing
 vim.keymap.set("n", "d", '"_d', { noremap = true, silent = true })
--- lua/mg/plugins/lsp.lua
+++ lua/mg/plugins/lsp.lua
@@ -1,6 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
+    -- consider using conform and mason
     "saghen/blink.cmp",
     {
       "folke/lazydev.nvim",
@@ -14,14 +15,47 @@ return {
   },
   config = function()
     local capabilities = require("blink.cmp").get_lsp_capabilities()
-    require("lspconfig").lua_ls.setup{ capabilities = capabilities }
+    require("lspconfig").ruff.setup {}
+
+    require("lspconfig").pyright.setup({
+      capabilities = capabilities,
+      settings = {
+        pyright = {
+          -- Using Ruff's import organizer
+          disableOrganizeImports = true,
+        },
+        python = {
+          analysis = {
+            -- Ignore all files for analysis to exclusively use Ruff for linting
+            ignore = { '*' },
+          },
+        },
+      },
+    })
+
+    require("lspconfig").lua_ls.setup { capabilities = capabilities }
+
     require("lspconfig").clangd.setup({
-      cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose', '--header-insertion=never'},
+      cmd = { 'clangd', '--background-index', '--clang-tidy', '--log=verbose', '--header-insertion=never' },
       init_options = {
         fallbackFlags = { '-std=c++20' },
       },
       capabilities = capabilities,
     })
-    require("lspconfig").pyright.setup{ capabilities = capabilities }
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client == nil then
+          return
+        end
+        if client.name == 'ruff' then
+          -- Disable hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+        end
+      end,
+      desc = 'LSP: Disable hover capability from Ruff',
+    })
   end,
 }
