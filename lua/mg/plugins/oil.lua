5512149de25d508e5de6f4092ee45956c938d169
MaGnsio <magnsi0o0@gmail.com>
2025-04-13 20:49:06 +0200

    oil 🛢

--- lua/mg/plugins/oil.lua
+++ lua/mg/plugins/oil.lua
@@ -6,14 +6,17 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local oil = require("oil")
-
     oil.setup({
       view_options = {
         -- Show files and directories that start with "."
         show_hidden = true,
-      }
+      },
+      -- Configuration for the floating window in oil.open_float
+      float = {
+        max_width = 0.5,
+        max_height = 0.5,
+      },
     })
-
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
     keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
