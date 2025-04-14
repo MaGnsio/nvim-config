a9e2fca604f2bf32bc4fae5a6c51a6de1bbae7e5
MaGnsi0 <mhefni@noon.com>
2025-04-14 13:56:54 +0200

    fix deprecated stuff 🚮

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-local start_time = vim.loop.hrtime()  -- Get the start time (in nanoseconds)
+local start_time = vim.uv.hrtime()  -- Get the start time (in nanoseconds)
 
 require("mg.core")
 require("mg.lazy")
 
-local loading_time = math.floor((vim.loop.hrtime() - start_time) / 1e6 + 0.5)
+local loading_time = math.floor((vim.uv.hrtime() - start_time) / 1e6 + 0.5)
 print("Startup Time: " .. loading_time .. "ms ⏳")
--- lua/mg/plugins/git.lua
+++ lua/mg/plugins/git.lua
@@ -6,7 +6,8 @@ return {
       gitsigns.setup()
 
       local keymap = vim.keymap
-      keymap.set("n", "<leader>gh", gitsigns.preview_hunk, { desc = "Preview the hunk at the cursor position in a floating window" })
+      keymap.set("n", "<leader>gh", gitsigns.preview_hunk,
+        { desc = "Preview the hunk at the cursor position in a floating window" })
       keymap.set("n", "<leader>gb", gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
     end,
   },
