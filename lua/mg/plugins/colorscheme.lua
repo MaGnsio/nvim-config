b7fb5661b17b39265cc27b860401aacab0dae265
MaGnsi0 <mhefni@noon.com>
2026-07-05 14:28:51 +0300

    change colorscheme

--- lua/mg/plugins/colorscheme.lua
+++ lua/mg/plugins/colorscheme.lua
@@ -1,12 +1,19 @@
+-- return {
+--   {
+--     "folke/tokyonight.nvim",
+--     name = "tokyonight",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {},
+--     config = function()
+--       vim.cmd.colorscheme("tokyonight-moon")
+--     end
+--   }
+-- }
 return {
-  {
-    "folke/tokyonight.nvim",
-    name = "tokyonight",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd.colorscheme("tokyonight-moon")
-    end
-  }
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine")
+	end
 }
