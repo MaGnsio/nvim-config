ceb2c8a2858766c67f01c5c05dfbd08c34b47529
Muhammed <magnsi0o0@gmail.com>
2026-09-09 23:44:29 +0300

    updating treesitter 🌲

--- lua/mg/plugins/treesitter.lua
+++ lua/mg/plugins/treesitter.lua
@@ -1,33 +1,39 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
-  dependencies = { "windwp/nvim-ts-autotag", },
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
   config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+    -- Automatically install missing parsers on enter
+    local parsers = {
+      "c", "cpp", "python", "go", "rust", "markdown", "markdown_inline",
+      "bash", "lua", "gitignore", "query", "vim", "vimdoc"
+    }
 
-    -- configure treesitter
-    treesitter.setup({
-      -- install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = true,
-      -- automatically install missing parsers when entering buffer
-      auto_install = false,
-      -- list of parsers to ignore installing (or "all")
-      ignore_install = {},
-      modules = {},
-      -- enable syntax highlighting
-      highlight = { enable = true, },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = { enable = true, },
-      -- ensure these language parsers are installed
-      ensure_installed = { "c", "cpp", "python", "go", "rust", "markdown", "markdown_inline", "bash", "lua", "gitignore", "query", "vim", "vimdoc", },
-      incremental_selection = {
-        enable = true,
-        keymaps = { init_selection = "<C-space>", node_incremental = "<C-space>", scope_incremental = false, node_decremental = "<bs>", },
-      },
+    -- Enable native treesitter syntax highlighting and indent per filetype
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
     })
+
+    -- Enable nvim-ts-autotag (now setup directly on its own module)
+    require("nvim-ts-autotag").setup()
+
+    -- Incremental Selection (Replaced treesitter.configs keymaps with native Neovim mappings)
+    vim.keymap.set("n", "<C-space>", function()
+      vim.treesitter.incremental_selection.init_selection()
+    end, { desc = "Init Treesitter Selection" })
+
+    vim.keymap.set("v", "<C-space>", function()
+      vim.treesitter.incremental_selection.node_incremental()
+    end, { desc = "Increment Treesitter Selection" })
+
+    vim.keymap.set("v", "<bs>", function()
+      vim.treesitter.incremental_selection.node_decremental()
+    end, { desc = "Decrement Treesitter Selection" })
   end,
 }
